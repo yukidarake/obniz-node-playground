b8b5f36c0bd046c804f01d4541e503eebfa6a86e
Toshiyuki Nakamura <yukidarake4@gmail.com>
2019-04-15 23:51:30 +0900

    Add ESLint

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const Obniz = require('obniz');
 
 const options = process.env.OBNIZ_ACCESS_TOKEN
   ? {
-      access_token: process.env.OBNIZ_ACCESS_TOKEN
-    }
+    access_token: process.env.OBNIZ_ACCESS_TOKEN,
+  }
   : {};
 const obniz = new Obniz(process.env.OBNIZ_ID, options);
-obniz.onconnect = async function() {
+obniz.onconnect = async () => {
   obniz.display.print('hello!');
 };
